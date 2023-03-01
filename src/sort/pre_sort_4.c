4bf04d2cf71fe71736b6262208417af37daf5dbd
inticoy <inticoy0406@gmail.com>
2023-03-01 19:23:25 +0900

    Refactor: optimize by eliminate trash command
    * eliminated rotate_a()

--- src/sort/pre_sort_4.c
+++ src/sort/pre_sort_4.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 13:32:31 by gyoon             #+#    #+#             */
-/*   Updated: 2023/03/01 19:02:12 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/03/01 19:19:15 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,16 @@
 
 void	pre_sort_4(t_stacks *stacks, t_order order)
 {
-	t_merge_info 	minfo;
+	t_merge_info	minfo;
 	int				idx;
 
-	rotate_a(stacks, ft_true);
 	push_b(stacks, ft_true);
 	rotate_b(stacks, ft_true);
 	minfo.a = 1;
 	minfo.b = 2;
 	minfo.c = 1;
 	minfo.order = order;
-	if (minfo.order > 0) // is asc->find biggest
+	if (minfo.order > 0)
 	{
 		if (*((int *)stacks->a->content) < *((int *)stacks->a->next->content))
 			swap_a(stacks, ft_true);
