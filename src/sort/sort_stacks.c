485119e09cf1a99ce0bb477031380e58edeed0c9
inticoy <inticoy0406@gmail.com>
2023-03-01 21:37:11 +0900

    Fix norm error
    * sort_stacks.c

--- src/sort/sort_stacks.c
+++ src/sort/sort_stacks.c
@@ -6,19 +6,15 @@
 /*   By: gyoon <gyoon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 16:54:29 by gyoon             #+#    #+#             */
-/*   Updated: 2023/03/01 20:26:06 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/03/01 21:36:50 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "push_swap.h"
 
-void	sort_stacks(t_stacks *stacks)
+static void	sort_2345(t_stacks *stacks)
 {
-	t_div_info	info;
-	int			i;
-	int			num_conquer;
-
 	if (stacks->num == 2)
 	{
 		sort_2(stacks, asc);
@@ -39,20 +35,32 @@ void	sort_stacks(t_stacks *stacks)
 		sort_5(stacks, asc);
 		return ;
 	}
+}
+
+void	sort_stacks(t_stacks *stacks)
+{
+	t_div_info	info;
+	int			i;
+	int			num_conquer;
+
+	if (2 <= stacks->num && stacks->num <= 5)
+	{
+		sort_2345(stacks);
+		return ;
+	}
 	info = get_div_info(ft_lstsize(stacks->a));
-	i = 0;
+	i = -1;
 	num_conquer = info.level;
 	if (info.level % 2 == 0)
 		reverse_orders(info.orders, info.part);
 	preprocess(stacks, info);
-	while (i < num_conquer)
+	while (++i < num_conquer)
 	{
 		if (i % 2 == 0)
 			merge_all_to_a(stacks, info);
 		else
 			merge_all_to_b(stacks, info);
 		info = update_div_info(info);
-		i++;
 	}
 	if (num_conquer % 2 == 0)
 		repeat_cmd(push_a, stacks, ft_true, stacks->num);
