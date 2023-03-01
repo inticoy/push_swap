0dd49cdaeee40b43cc0ec96c192dd9937fe95eb5
inticoy <inticoy0406@gmail.com>
2023-03-01 15:37:51 +0900

    Fix pre_sort_2
    * pre_sort_2 arrange if statements

--- src/sort/pre_sort_2.c
+++ src/sort/pre_sort_2.c
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 13:29:58 by gyoon             #+#    #+#             */
-/*   Updated: 2023/03/01 15:32:26 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/03/01 15:37:14 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,10 @@
 
 void	pre_sort_2(t_stacks *stacks, t_order order)
 {
-	int	x;
-	int	y;
+	const int	x = *((int *)stacks->a->content);
+	const int	y = *((int *)stacks->a->next->content);
 
-	x = *((int *)stacks->a->content);
-	y = *((int *)stacks->a->next->content);
-	if (order > 0 && x < y)
-		swap_a(stacks, ft_true);
-	else if (order < 0 && x > y)
+	if ((order > 0 && x < y) || (order < 0 && x > y))
 		swap_a(stacks, ft_true);
 	repeat_cmd(push_b, stacks, ft_true, 2);
 }
